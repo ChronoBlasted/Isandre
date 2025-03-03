2698979b9159f0c8305e82642f3355f846e63913
Sebastien chevallier <s.chevallier.pro@gmail.com>
2025-03-03 16:57:55 +0100

    Merge

--- Assets/Scripts/Gameplay/PowerUp/PowerUpTireRateIncrease.cs
+++ Assets/Scripts/Gameplay/PowerUp/PowerUpTireRateIncrease.cs
@@ -10,3 +10,16 @@ public class PowerUpTireRateIncrease : PowerUp
         PlayerManager.Instance.playerWeapon.currentWeapon.weaponData.attackRate = PlayerManager.Instance.playerWeapon.currentWeapon.weaponData.attackRate * attackRateMultiplier;
     }
 }
+
+[CreateAssetMenu(fileName = "Scriptable Objects", menuName = "PowerUp/Explosive", order = 1)]
+public class Explosive : PowerUp
+{
+    [SerializeField] private float explosionRadius = 2f;
+    [SerializeField] private float explosionDamage = 5f;
+    [SerializeField] private float explosionForce = 5f;
+    public override void OnUse()
+    {
+        base.OnUse();
+        PlayerManager.Instance.playerWeapon.RegisterBulletDecorator<ExplosiveProjectileDecorator>(explosionRadius, explosionDamage, explosionForce);
+    }
+}
