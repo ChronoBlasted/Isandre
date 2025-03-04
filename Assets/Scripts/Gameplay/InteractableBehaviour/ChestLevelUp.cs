39cc0422f1bb3a3e0da5c6890188ba2657bee9a7
OMAILLOT <orianmaillot@gmail.com>
2025-03-04 10:05:25 +0100

    feat: change color card, chest feedback, chest good spawn

--- Assets/Scripts/Gameplay/InteractableBehaviour/ChestLevelUp.cs
+++ Assets/Scripts/Gameplay/InteractableBehaviour/ChestLevelUp.cs
@@ -1,5 +1,6 @@
 using MoreMountains.Feedbacks;
 using NUnit.Framework;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -18,17 +19,24 @@ public class ChestLevelUp : InteractableBehaviour
     public override void OnInteract()
     {
         base.OnInteract();
+        StartCoroutine(WaitToOpen());   
+    }
+    IEnumerator WaitToOpen()
+    {
+        selectedFeedBack.PlayFeedbacks();
+
+        yield return new WaitForSeconds(data.timeToOpen);
+
         List<PowerUp> newPowerUp = new()
         {
             data.basicPowerUp[0],
             data.rarePowerUp[0],
             data.epicPowerUp[0],
         };
+
         UIManager.Instance.LevelUpPopup.SetPowerUp(newPowerUp);
         UIManager.Instance.AddPopup(UIManager.Instance.LevelUpPopup);
-
-        
-        //SpawnUi and selected powerup
+        Destroy(this);
     }
     private void OnTriggerEnter(Collider other)
     {
